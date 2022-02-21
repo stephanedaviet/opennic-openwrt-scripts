769d80516044ab3984b71aa79f4d8948c9dedefa
Stéphane Daviet <stephanedaviet@users.noreply.github.com>
2022-02-21 12:19:49 +0100

    Fix the broken script

--- opennic-dnsmasq-geoip-sync.sh
+++ opennic-dnsmasq-geoip-sync.sh
@@ -3,20 +3,19 @@
 set -e
 set -o pipefail
 
-logger "Starting synchronisation of OpenNIC DNS servers with DNSMasq..."
+logger "Starting synchronisation of OpenNIC DNS servers..."
 
-wget -qO /tmp/opennic-geoip-servers https://api.opennicproject.org/geoip/
-
-sed -i 's/#.*//g' /tmp/opennic-geoip-servers
-
-uci del dhcp.@dnsmasq[-1].server
-
-cat /tmp/opennic-geoip-servers | while read serverIp ; do
-    uci add_list dhcp.@dnsmasq[-1].server=$serverIp ;
-done
-
-uci commit dhcp
-
-killall -s SIGHUP dnsmasq
-
-logger "Synchronisation of OpenNIC DNS servers with DNSMasq successfully done"
+# Enable ISP DNS servers (to be able to resolve opennicproject.org)
+# Another and more privacy friendy option is to access the API directly with the server IP
+uci set network.wan.peerdns='1'
+uci commit network        
+/etc/init.d/network reload     
+                                                                                               
+# Disable ISP DNS servers (only safe way to disable ISP, DNSMasq DNS forwarding doesn't really work)
+uci set network.wan.peerdns='0'                                                                
+# Set DNS servers (uci set and not uci add_list as stated in some eroneous docs)               
+uci set network.wan.dns="`curl "https://api.opennicproject.org/geoip/?bare&pct=90&bl" | xargs`"
+uci commit network                                               
+/etc/init.d/network reload                                       
+                                                                 
+logger "Synchronisation of OpenNIC DNS servers successfully done"
